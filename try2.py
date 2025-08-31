e6d9803aea29aea43eff105aa8eb333fdbc53f56
Nakshatra1729yuvi <nakshatrayadav1729@gmail.com>
2025-08-31 17:02:24 +0530

    try2.py

--- try2.py
+++ try2.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import math
 
-img=cv2.imread('UAS DTU\\task_images\\9.png')
+img=cv2.imread('UAS DTU\\task_images\\10.png')
 
 
 img_gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -12,10 +12,20 @@ template_square=cv2.imread('square.png',0)
 template_circle=cv2.imread('circle.png',0)
 
 
-star=[[],[],[]]
+star=[[],[],[]]              #In order green , red , yellow
 triangle=[[],[],[]]
 square=[[],[],[]]
+circle=[]
+circle_arranged=[None]*3
 
+
+def circle_colour(color):
+    if color[0]>=210 and color[1]>=210 and color[2]>=210:
+        return 2     # for grey
+    elif color[0]>=210 and color[1]<=210 and color[2] >= 210:
+        return 1     # for pink
+    else:
+        return 0      # for blue
 
 
 def check_colour(color):
@@ -26,6 +36,10 @@ def check_colour(color):
     else:
         return 2        #2 for yellow
 
+def arrange_circle():
+    for t in circle:
+        order=circle_colour(img[t[0],t[1]])
+        circle_arranged[order]=t
 
 
 
@@ -39,10 +53,21 @@ def get_cicles():
 
     # print(loc[::-1])
     for pt in zip(*loc[::-1]):
-        cv2.rectangle(img,pt,(pt[0]+w,pt[1]+h),(0,255,0),1)
+        #cv2.rectangle(img,pt,(pt[0]+w,pt[1]+h),(0,255,0),1)
         cx,cy=pt[0]+w//2,pt[1]+h//2
         c=[cy.item(),cx.item()]
-        print(img[cy,cx])
+        #print(img[cy,cx])
+        a=True
+        for t in circle:
+            if math.sqrt(((t[0]-c[0])**2)+((t[1]-c[1])**2))<=100:
+                a=False
+                break
+            else:
+                a=True
+        if a:
+            circle.append(c)
+            cv2.rectangle(img,pt,(pt[0]+w,pt[1]+h),(0,255,0),1)
+            print(img[cy,cx])
 
 
 
@@ -87,7 +112,7 @@ print(star)
 print(triangle) 
 print(square) 
 
-get_cicles()
+
 
 print(len(star[0])+len(star[1])+len(star[2]))
 print(len(triangle[0])+len(triangle[1])+len(triangle[2]))
@@ -95,7 +120,9 @@ print(len(square[0])+len(square[1])+len(square[2]))
 
 
 get_cicles()
-
+print(circle)
+arrange_circle()
+print(circle_arranged)
 
 cv2.imshow('Final',img)
 
